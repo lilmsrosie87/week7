1619d13fc28353c1eccb3a5217500fe1b76d6273
Roslyn Harris <rharris0113@gmail.com>
2018-01-04 22:11:43 -0800

    created for class work

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$(".fadeOut").click(function(){
+		$("img").hide();
+	});
+});
+
+$(document).ready(function(){
+	$(".fadeIn").click(function(){
+		$("img").show();
+	});
+});
+
+$(document).ready(function(){
+	$("h1").dblclick(function(){
+		$("div").css("background-color", "#ff6666");
+	});
+});
+
+// to do list
+ $("button").click(function(){
+	 var = newTask $("#input")
+	
+	$(".list").click(function(){
+		$("ul").append("<li>""</li>");
+	});
+});
+
+// be sure to work on excercises here http://codifyacademy.com/wk6/
+// you stopped at number 8
+// update Felicia's website
